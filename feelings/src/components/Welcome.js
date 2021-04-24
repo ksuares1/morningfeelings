ada9c082530717d77d1eb2366512c0403ab7de92
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-04-24 03:07:59 -0400

    Added button to welcome page

--- feelings/src/components/Welcome.js
+++ feelings/src/components/Welcome.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+// import button from "bootstrap";
 const WelcomeComponent=()=>{
     return(
         <main>
             <div>
                 <h1>Morning Feelings</h1>
+                <button type="button" class="btn btn-dark">Welcome</button>
             </div>
         </main>
     )
